c870cd11343ea336c4adeb23a10ad9d67219005a
tiffany <tenghuanjun@foxmail.com>
2026-06-09 10:47:40 +0800

    修复页面打开报错

--- src-tauri/build.rs
+++
@@ -1,7 +0,0 @@
-fn main() {
-    tauri_build::try_build(
-        tauri_build::Attributes::new()
-            .app_manifest(tauri_build::AppManifest::new().commands(&["open_quick_login"])),
-    )
-    .expect("tauri-build failed");
-}
